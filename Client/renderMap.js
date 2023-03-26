660b2cb780281a71b6a966f8c7fc26c294d1df94
IlerraS <llerra.suerte@gmail.com>
2023-03-26 03:05:40 -0500

    ADD: distance analysis and gradual buffer change

--- Client/renderMap.js
+++ Client/renderMap.js
@@ -1,21 +1,24 @@
 import { getPoints } from './getPoints.js';
-let coords;
+let userCoords;
 let map;
+let userMarker;
+
 
 const locateUser = (position) =>
 {
     const { latitude, longitude } = position.coords;
     //user coordinates
-    coords = [latitude, longitude];
-    console.log(coords);
+    userCoords = [latitude, longitude];
+    console.log(userCoords);
     if(!map)
     {
-        map = L.map('map').setView(coords, 30);
+        map = L.map('map').setView(userCoords, 30);
     }
     else
     {
-        map.setView(coords, 20)
+        map.setView(userCoords, 20)
     }
+    userMarker = L.marker(userCoords).addTo(map);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
@@ -23,7 +26,7 @@ const locateUser = (position) =>
     //creating markers
     getPoints.forEach(point =>
     {
-        L.circle([point[0], point[1]],
+        const circle = L.circle([point[0], point[1]],
             {
                 radius: 30,
                 stroke: false,
@@ -31,18 +34,24 @@ const locateUser = (position) =>
                 fillOpacity: 0.5
             })
             .addTo(map);
+
+        const distanceToCenter = userMarker.getLatLng().distanceTo(circle.getLatLng());
+
+        const gradientScale = chroma.scale(['#00ffff', '#ff0000']).domain([0, circle.getRadius()]);
+
+        if (distanceToCenter <= circle.getRadius()) {
+            circle.setStyle({color: 'green', fillColor: '#0f3'});
+        } else {
+            const colorValue = gradientScale(distanceToCenter).hex();
+            circle.setStyle({fillColor: colorValue});
+        }
     });
 }
-
-
 
 const fail = () =>
 {
     alert('Could not get your position');
 }
-
-
-
 
 
 export {locateUser, fail};
